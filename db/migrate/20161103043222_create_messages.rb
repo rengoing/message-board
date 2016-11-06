8306e8bfe20ef5b1b40a5df296f67c0e636da092
Toshiyuki Watanabe <basssisthinatch@gmail.com>
2016-11-06 23:45:06 +0000

    database modified

--- db/migrate/20161103043222_create_messages.rb
+++ db/migrate/20161103043222_create_messages.rb
@@ -3,7 +3,9 @@ class CreateMessages < ActiveRecord::Migration
     create_table :messages do |t|
       t.string :name
       t.string :body
-
+      t.string :age
+      t.string :messages
+      t.string :integer
       t.timestamps null: false
     end
   end
